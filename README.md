1371568ae654b713d961f9ce925ab1268bb734c2
Eliiah <alyawka@gmail.com>
2022-07-23 10:47:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Some Blog attempts
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
+  
+  resources :posts do
+    resources :comments
+  end
+
+  get 'about', to: 'pages#about'
+  root "pages#home"
+end
