426a37ec14461a6528c078ac589b3b009a349c86
Eliiah <alyawka@gmail.com>
2022-08-17 18:41:13 +0300

    bugfix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,12 @@
+module YourApp
+    class Application < Rails::Application
+      config.after_initialize do
 Rails.configuration.stripe = { 
     :publishable_key => 'pk_test_51LVdU5FSVR4ZEBnz06KloGnzbq4se5jtrN1ImjvQqNe3Ickn85wX0EmfUJmuQTguThwCk4N1n5Z8nvgNmtEFmoIi00yie34Q1a', 
     :stripe_api_key => 'sk_test_51LVdU5FSVR4ZEBnzC47bCFtAUmp09O4J9Mv911Mcero6IU4mOXTlTIk1UCPFyCcOwHukHIcY08FBc7A149NVm1Jt00xG2zYmp7'
   }   
 
-Stripe.api_key = Rails.configuration.stripe[:stripe_api_key]
+  Stripe.api_key = Rails.application.credentials[:publishable_key][:stripe_api_key]
+        end
+    end
+end
