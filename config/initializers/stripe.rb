aed6e8286948137985fad2d89f36f9514478f98d
Eliiah <alyawka@gmail.com>
2022-08-17 19:13:24 +0300

    bugfix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
     :publishable_key => Rails.application.secrets.publishable_key,
-    :stripe_api_key  => Rails.application.secrets.secret_key
+    :secret_key => Rails.application.secrets.secret_key
     }
  
-Stripe.api_key = Rails.configuration.stripe[:stripe_api_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
